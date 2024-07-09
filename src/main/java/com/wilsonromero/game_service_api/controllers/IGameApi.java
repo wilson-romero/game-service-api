fc74459fc87e2783ed95f54656dc21a4ceaa51a6
Wilson Romero <wilson.romero@gmail.com>
2024-07-09 05:04:46 -0500

    ✨: Add getGames with pagination and sort criteria

--- src/main/java/com/wilsonromero/game_service_api/controllers/IGameApi.java
+++ src/main/java/com/wilsonromero/game_service_api/controllers/IGameApi.java
@@ -2,8 +2,11 @@ package com.wilsonromero.game_service_api.controllers;
 
 import com.wilsonromero.game_service_api.commons.constans.ApiPathVariables;
 import com.wilsonromero.game_service_api.commons.entities.Game;
+import io.swagger.v3.oas.annotations.Parameter;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 
 @RequestMapping(ApiPathVariables.V1_ROUTE + ApiPathVariables.GAMES_ROUTE)
 public interface IGameApi {
@@ -12,4 +15,12 @@ public interface IGameApi {
 
     @GetMapping("/{id}")
     ResponseEntity<Game> getGameById(@PathVariable Long id);
+
+    @GetMapping
+    ResponseEntity<Page<Game>> getGames(
+            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "size", defaultValue = "20") int size,
+            @Parameter(description = "Sorting criteria in the format: property:(asc|desc). Default sort order is ascending. Multiple sort criteria are supported.")
+            @RequestParam(name = "sort", required = false) String[] sort
+    );
 }
